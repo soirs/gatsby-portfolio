8cff61d78deefdb4274b65c1f58b7833f4291dbe
Frank Richard Semakula <franksemakula@gmail.com>
2018-08-23 16:27:23 +0200

    Updates

---
+++ path----7e505a31202682862c5d.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{108:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Frank Richard Semakula"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----7e505a31202682862c5d.js.map
